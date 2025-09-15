43754bcc8b9b3f4042d643bd8433a54be632066f
armanabramov <128611537+armanabramov@users.noreply.github.com>
2025-09-15 22:20:38 +0300

    feat: add handler on click button

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
+import { useActionState, useState } from 'react';
 import styles from './App.module.css';
 
 function App() {
+	const [value, setValue] = useState('');
+	//const [list , setList ] = useState([]);
+	const [error , setError] = useState('');
+	const [isValueVaild, setIsValueVaild]  = useState(false);
+
+	const onInputButtonClick = () => {
+		const promptValue = prompt();
+		if (promptValue.length >= 3) {
+			setValue(promptValue);
+			setError('');
+			setIsValueVaild(true);
+		}	else {
+			setError('Введенное значение должно содержать минимум 3 символа');
+			setIsValueVaild(false);
+		}
+	}
+
 	return (
 		<>
 			<div className={styles.app}>
 				<h1 className={styles['page-heading']}>Ввод значения</h1>
 				<p className={styles['no-margin-text']}>
-					Текущее значение <code>value</code>: "<output className={styles['current-value']}></output>"
+					Текущее значение <code>value</code>: "<output className={styles['current-value']}>{value}</output>"
 				</p>
-				<div className={styles.error}>Введенное значение должно содержать минимум 3 символа</div>
+				{error && <div className={styles.error}>{error}</div>}
 				<div className={styles['buttons-container']}>
-					<button className={styles.button}>Ввести новое</button>
-					<button className={styles.button} disabled>
+					<button className={styles.button} onClick={onInputButtonClick}>Ввести новое</button>
+					<button className={styles.button} disabled={!isValueVaild}>
 						Добавить в список
 					</button>
 				</div>
